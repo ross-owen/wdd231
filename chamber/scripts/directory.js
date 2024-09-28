ca9450257d311a39d79cbe2e86a0255bb3eb5696
Ross L. Owen <ross.lloyd.owen@gmail.com>
2024-09-28 11:51:13 -0600

    week2: chamber/directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,50 @@
+﻿const cards = document.querySelector('#cards');
+
+async function getMemberData() {
+    const response = await fetch('https://ross-owen.github.io/wdd231/chamber/scripts/members.json');
+    const data = await response.json();
+    displayMembers(data)
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        const card = document.createElement('section');
+        card.classList.add("member");
+
+        const logo = document.createElement('img');
+        logo.setAttribute('src', member.imageUrl);
+        logo.setAttribute('alt', `${member.name}`);
+        logo.setAttribute('width', '320');
+        logo.setAttribute('height', '180');
+
+        const businessName = document.createElement('h2');
+        businessName.textContent = `${member.name}`;
+
+        const address = document.createElement('div');
+        address.textContent = member.address;
+        
+        const phone = document.createElement('div');
+        phone.textContent = member.phone;
+        
+        const website = document.createElement("div");
+        const webLink = document.createElement('a');
+        webLink.href = member.websiteUrl;
+        webLink.textContent = member.websiteUrl;
+        website.appendChild(webLink);
+        
+        const membership = document.createElement('div');
+        membership.textContent = `Membership: ${member.membershipLevel}`;
+
+
+        card.appendChild(logo);
+        card.appendChild(businessName);
+        card.appendChild(address);
+        card.appendChild(phone)
+        card.appendChild(website)
+        card.appendChild(membership)
+
+        cards.appendChild(card);
+    })
+}
+
+getMemberData();
