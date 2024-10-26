b2a3f06d5b05e6eca2b41f3b6d4f0ec250cd8c01
Ross L. Owen <ross.lloyd.owen@gmail.com>
2024-10-26 15:23:11 -0600

    week6: export search flights

--- project/scripts/flight-search.js
+++ project/scripts/flight-search.js
@@ -1,41 +1,5 @@
-const form = document.getElementById('search-form');
-const originAirport = document.getElementById('from-airport');
-const destinationAirport = document.getElementById('to-airport');
-const departureDate = document.getElementById('departure-date');
-const returnDate = document.getElementById('return-date');
-const flightSearchResults = document.getElementById('flight-search-results');
-
-// const token = 'duffel_test_KEbi9bubp8Keb-K9Tt6Id-XFP_tPDRqBkAa8bVIkJXz';
-// const offerUrl = 'https://api.duffel.com/air/offer_requests?supplier_timeout=2000';
-let searchResults = [];
-
-document.getElementById('search-flights')
-    .addEventListener('click', function (event) {
-
-        let depart = parseISOLocal(departureDate.value);
-        departureDate.setCustomValidity(depart < new Date() ? 'Departure date is invalid' : '');
-        let arrive = parseISOLocal(returnDate.value);
-        returnDate.setCustomValidity(arrive <= depart ? 'Return date is invalid' : '');
-        
-        if (!form.checkValidity()) {
-            form.reportValidity();
-        } else {
-            event.preventDefault();
-            searchFlights().then(searchFlights => {
-                // hide spinner?
-                // enable button
-                console.log('done');
-            });
-        }
-    });
-
-function parseISOLocal(dateAsString) {
-    let [y, m, d] = dateAsString.split(/\D/);
-    return new Date(y, --m, d);
-}
-
-async function searchFlights() {
-    searchResults = [];
+export async function searchFlights(flightSearchResults) {
+    let searchResults = [];
     // let request = new Request(offerUrl, getOptions());
     const response = await fetch('https://ross-owen.github.io/wdd231/project/scripts/flight-info.json');
     if (response.status === 200) {
@@ -48,7 +12,7 @@ async function searchFlights() {
                 destinationCity: leavingFlight.destination.city_name,
                 departureDate: new Date(leavingFlight.departing_at),
                 returningDate: new Date(returningFlight.departing_at),
-                layoverCity: 'San Francisco, CA', // default layover city
+                layoverCity: 'San Francisco', // default layover city
                 totalPrice: offer.total_amount,
             };
             if (offer.slices[0].segments.length > 1) {
@@ -77,46 +41,51 @@ async function searchFlights() {
             `;
             flightSearchResults.appendChild(row);
         }
+        return searchResults;
     }
 }
 
-function createSearchRequest(fromAirport, toAirport, departureDate, returnDate) {
-    return JSON.stringify({
-        data: {
-            slices: [
-                {
-                    origin: fromAirport,
-                    destination: toAirport,
-                    departure_date: departureDate
-                },
-                {
-                    origin: toAirport,
-                    destination: fromAirport,
-                    departure_date: returnDate
-                }
-            ],
-            passengers: [
-                {
-                    type: 'adult'
-                }
-            ],
-            max_connections: 2,
-            cabin_class: 'economy'
-        }
-    });
-}
-
-function getOptions() {
-    return {
-        mode: 'no-cors',
-        method: 'POST',
-        headers: {
-            'Accept-Encoding': 'gzip',
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Duffel-Version': 'v2',
-            'Authorization': `Bearer ${token}`
-        },
-        body: createSearchRequest(originAirport.value, destinationAirport.value, departureDate.value, returnDate.value)
-    };
-}
+// TODO: save this for later when we are able call duffel directly
+// const token = 'duffel_test_KEbi9bubp8Keb-K9Tt6Id-XFP_tPDRqBkAa8bVIkJXz';
+// const offerUrl = 'https://api.duffel.com/air/offer_requests?supplier_timeout=2000';
+//
+// function createSearchRequest(fromAirport, toAirport, departureDate, returnDate) {
+//     return JSON.stringify({
+//         data: {
+//             slices: [
+//                 {
+//                     origin: fromAirport,
+//                     destination: toAirport,
+//                     departure_date: departureDate
+//                 },
+//                 {
+//                     origin: toAirport,
+//                     destination: fromAirport,
+//                     departure_date: returnDate
+//                 }
+//             ],
+//             passengers: [
+//                 {
+//                     type: 'adult'
+//                 }
+//             ],
+//             max_connections: 2,
+//             cabin_class: 'economy'
+//         }
+//     });
+// }
+//
+// function getOptions() {
+//     return {
+//         mode: 'no-cors',
+//         method: 'POST',
+//         headers: {
+//             'Accept-Encoding': 'gzip',
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json',
+//             'Duffel-Version': 'v2',
+//             'Authorization': `Bearer ${token}`
+//         },
+//         body: createSearchRequest(originAirport.value, destinationAirport.value, departureDate.value, returnDate.value)
+//     };
+// }
--- project/scripts/search.js
+++ project/scripts/search.js
@@ -1,20 +1,59 @@
 import { searchAirports } from './airport-search.js';
+import { searchFlights } from './flight-search.js';
 
-const searchBox = document.getElementById('search-box');
-const searchButton = document.getElementById('search-button');
+// search for airport fields
+const searchAirportBox = document.getElementById('search-box');
+const searchAirportButton = document.getElementById('search-button');
 const searchAirportDialog = document.getElementById('search-dialog');
 const fromAirport = document.getElementById('from-airport');
 const toAirport = document.getElementById('to-airport');
 const airportType = document.getElementById('search-city');
 const airportSearchResults = document.getElementById('airport-search-results');
+
+// search for flights fields
+const form = document.getElementById('search-form');
+const originAirport = document.getElementById('from-airport');
+const destinationAirport = document.getElementById('to-airport');
+const departureDate = document.getElementById('departure-date');
+const returnDate = document.getElementById('return-date');
+const flightSearchResults = document.getElementById('flight-search-results');
+
+// init
 let searchingFor = fromAirport;
 
-searchButton.addEventListener('click', () => searchAirports(airportSearchResults, searchBox.value, selectAirport));
+// event handling
+searchAirportButton
+    .addEventListener('click', () => searchAirports(airportSearchResults, searchAirportBox.value, selectAirport));
 
 document.getElementById('search-from')
     .addEventListener('click', () => showAirportSearch('Departure'));
 document.getElementById('search-to')
     .addEventListener('click', () => showAirportSearch('Destination'));
+
+document.getElementById('search-flights')
+    .addEventListener('click', function (event) {
+
+        let depart = parseISOLocal(departureDate.value);
+        departureDate.setCustomValidity(depart < new Date() ? 'Departure date is invalid' : '');
+        let arrive = parseISOLocal(returnDate.value);
+        returnDate.setCustomValidity(arrive <= depart ? 'Return date is invalid' : '');
+
+        if (!form.checkValidity()) {
+            form.reportValidity();
+        } else {
+            event.preventDefault();
+            searchFlights(flightSearchResults).then((found) => {
+                // hide spinner?
+                // enable button
+                console.log(found);
+            });
+        }
+    });
+
+function parseISOLocal(dateAsString) {
+    let [y, m, d] = dateAsString.split(/\D/);
+    return new Date(y, --m, d);
+}
 
 function selectAirport(airportCode) {
     searchingFor.value = airportCode;
@@ -23,7 +62,7 @@ function selectAirport(airportCode) {
 
 function showAirportSearch(fromTo) {
     airportType.innerHTML = fromTo;
-    searchBox.value = '';
+    searchAirportBox.value = '';
     if (fromTo === 'Departure') {
         searchingFor = fromAirport;
     } else {
