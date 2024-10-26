3a2d7a3c6e9329f45fc92a57450a0d33b78d8592
Ross L. Owen <ross.lloyd.owen@gmail.com>
2024-10-26 13:08:47 -0600

    week6: fake data from github

--- project/scripts/flight-search.js
+++ project/scripts/flight-search.js
@@ -3,9 +3,11 @@ const originAirport = document.getElementById('from-airport');
 const destinationAirport = document.getElementById('to-airport');
 const departureDate = document.getElementById('departure-date');
 const returnDate = document.getElementById('return-date');
+const flightSearchResults = document.getElementById('flight-search-results');
 
-const token = 'duffel_test_KEbi9bubp8Keb-K9Tt6Id-XFP_tPDRqBkAa8bVIkJXz';
-const offerUrl = 'https://api.duffel.com/air/offer_requests?supplier_timeout=2000';
+// const token = 'duffel_test_KEbi9bubp8Keb-K9Tt6Id-XFP_tPDRqBkAa8bVIkJXz';
+// const offerUrl = 'https://api.duffel.com/air/offer_requests?supplier_timeout=2000';
+let searchResults = [];
 
 document.getElementById('search-flights')
     .addEventListener('click', function (event) {
@@ -33,12 +35,48 @@ function parseISOLocal(dateAsString) {
 }
 
 async function searchFlights() {
+    searchResults = [];
     // let request = new Request(offerUrl, getOptions());
     const response = await fetch('https://ross-owen.github.io/wdd231/project/scripts/flight-info.json');
     if (response.status === 200) {
         const result = await response.json();
-        // TODO: do stuff with the response
-        console.log(result);
+        for (const offer of result.data.offers) {
+            let leavingFlight = offer.slices[0].segments[0];
+            let returningFlight = offer.slices[1].segments[0];
+            let itinerary = {
+                originCity: leavingFlight.origin.city_name,
+                destinationCity: leavingFlight.destination.city_name,
+                departureDate: new Date(leavingFlight.departing_at),
+                returningDate: new Date(returningFlight.departing_at),
+                layoverCity: 'San Francisco, CA', // default layover city
+                totalPrice: offer.total_amount,
+            };
+            if (offer.slices[0].segments.length > 1) {
+                let layover = offer.slices[0].segments[1];
+                itinerary.layoverCity = layover.origin.city_name;
+                itinerary.destinationCity = layover.destination.city_name;
+            }
+            searchResults.push(itinerary);
+
+            let row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${itinerary.originCity}</td>
+                <td>
+                    ${itinerary.departureDate.toLocaleDateString('en-US')}
+                    <br>
+                    ${itinerary.departureDate.toLocaleTimeString('en-US')}                    
+                </td>
+                <td>${itinerary.destinationCity}</td>
+                <td>
+                    ${itinerary.returningDate.toLocaleDateString('en-US')}
+                    <br>
+                    ${itinerary.returningDate.toLocaleTimeString('en-US')}                    
+                </td>
+                <td>${itinerary.layoverCity}</td>
+                <td>$${itinerary.totalPrice ?? '0.00'}</td>
+            `;
+            flightSearchResults.appendChild(row);
+        }
     }
 }
 
@@ -81,57 +119,4 @@ function getOptions() {
         },
         body: createSearchRequest(originAirport.value, destinationAirport.value, departureDate.value, returnDate.value)
     };
-}
-
-const searchResponse = {
-    "data": {
-        "offers": [
-            {
-                "base_amount": "418.20",
-                "tax_amount": "75.28",
-                "total_amount": "493.48",
-                "owner": {
-                    "name": "Duffel Airways"
-                },
-                "slices": [
-                    {
-                        "segments": [
-                            {   // flight out
-                                "departing_at": "2025-01-19T13:56:00",
-                                "arriving_at": "2025-01-19T18:01:00",
-                                "operating_carrier_flight_number": "5524",
-                                "duration": "PT7H5M",
-                                "origin": {
-                                    "name": "Salt Lake City International Airport",
-                                    "iata_code": "SLC",
-                                    "city_name": "Salt Lake City"
-                                },
-                                "destination": {
-                                    "name": "Lihue Airport",
-                                    "iata_code": "LIH",
-                                    "city_name": "Lihue"
-                                }
-                            },
-                            {   // return flight
-                                "departing_at": "2025-01-28T20:54:00",
-                                "arriving_at": "2025-01-29T06:59:00",
-                                "operating_carrier_flight_number": "5524",
-                                "duration": "PT7H5M",
-                                "origin": {
-                                    "name": "Lihue Airport",
-                                    "iata_code": "LIH",
-                                    "city_name": "Lihue"
-                                },
-                                "destination": {
-                                    "name": "Salt Lake City International Airport",
-                                    "iata_code": "SLC",
-                                    "city_name": "Salt Lake City"
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
 }
